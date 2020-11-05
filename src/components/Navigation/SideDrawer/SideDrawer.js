e99cfe6319cfb61b06cf48bd2e06d7555aec98fa
Nenad Novovic <novovicnenad96@gmail.com>
2020-11-05 15:20:29 +0100

    burger builder app 1st commit

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Auxiliary from "../../../hoc/Auxiliary/Auxiliary";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import classes from "./SideDrawer.css";
+
+function SideDrawer(props) {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+  return (
+    <Auxiliary>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Auxiliary>
+  );
+}
+
+export default SideDrawer;
