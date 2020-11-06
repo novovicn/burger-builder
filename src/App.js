2952701964133341316ddf1e655d9bce743889f5
Nenad Novovic <novovicnenad96@gmail.com>
2020-11-06 12:06:17 +0100

    added firebase db to store orders, and to provide ingredients

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 
 class App extends Component {
+
+
   render() {
     return (
       <div>
-        
         <Layout>
           <BurgerBuilder/>
         </Layout>
-        
       </div>
     );
   }
---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from './Spinner.css'
+
+function Spinner() {
+    return (
+        <div className={classes.Loader}>Loading...</div>
+    )
+}
+
+export default Spinner
